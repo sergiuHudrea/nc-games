3dcb3874a91de2255f76dde38060d990217e66e6
sergiuHudrea <sergiu.hudrea@kpmg.co.uk>
2023-01-16 20:04:42 +0000

    fixed login async localStorage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,12 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { UserContext } from './UserContext';
 
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
 
 export const Header = () => {
     const {value, setValue}= useContext(UserContext);
-    const loginImg = window.localStorage.getItem("loginImg")
-    const loginName = window.localStorage.getItem("loginName")
-    
-    
+    const [loginImg, setLoginImg] = useState(window.localStorage.getItem("loginImg"));
+    const [loginName, setLoginName] = useState(window.localStorage.getItem("loginName"));
 
     return <section>
         <h1 className="Header"> House Of Board Games </h1>
@@ -20,10 +18,12 @@ export const Header = () => {
         loginName ? loginName : "Guest"}</strong>
         </div> 
         <button className="Log_Button" onClick={() => {
-            window.localStorage.removeItem("loginImg")
-            window.localStorage.removeItem("loginName")
-            window.localStorage.removeItem("loginUsername")
-            setValue("")}}> Log out </button> 
+            window.localStorage.removeItem("loginImg");
+            window.localStorage.removeItem("loginName");
+            window.localStorage.removeItem("loginUsername");
+            setLoginImg('');
+            setLoginName('');
+            setValue(() => "")}}> Log out </button> 
         <button className="Log_Button"><Link to='/users' > Log in </Link></button>
     </section>
 }
