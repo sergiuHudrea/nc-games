3d2f01a2f653ee974fda56114c83ba04982f2e9c
sergiuHudrea <sergiu.hudrea@kpmg.co.uk>
2023-01-17 12:02:42 +0000

    repair delete to work when posting and deleting without refresh

--- src/components/RemoveComment.jsx
+++ src/components/RemoveComment.jsx
@@ -1,39 +1,32 @@
-import { useState } from "react";
 import { deleteComment } from "../api";
 
 
-
 export const RemoveComment = ({ comment, setComments }) => {
-    const [error, setError] = useState(false);
     const deletedComments = [];
-
 
 
     const handleClick = (e) => {
         e.preventDefault();
-        setError(false);
 
         setComments((currComments) => {
             const updatedComments = [...currComments];
             deletedComments.push({comment_id: comment.comment_id,
                      body: comment.body});
             return updatedComments.filter((comm) => comm.comment_id !== comment.comment_id);
-            
         })
 
-        deleteComment(comment.comment_id).then(() => {
-            alert('Comment deleted succesfully.');
-        }).catch(() => {
-            setError(true);
-            setComments((currComments) => {
-                const updatedComments = [...currComments];
-                return deletedComments.concat(updatedComments);
-            });
+        deleteComment(comment.comment_id)
+            .then(() => {
+                alert('Comment deleted succesfully.')})
+            .catch(() => {
+                setComments((currComments) => {
+                    const updatedComments = [...currComments];
+                    return deletedComments.concat(updatedComments);
+                });
+                alert("The comment was not deleted, please refresh.");
         }
         );
     };
-       
-    if (error) {return <p> Error, the deletion was not successful </p>};
 
     
 
